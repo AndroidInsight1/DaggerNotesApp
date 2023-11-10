1a7fcb56651854c723d7a31a2866972927452a44
Android Insight <insight.droid1@gmail.com>
2023-11-11 00:17:42 +0530

    third commit

--- app/src/main/java/com/example/notesappdagger/MyApplication.kt
+++ app/src/main/java/com/example/notesappdagger/MyApplication.kt
@@ -15,6 +15,7 @@ open class MyApplication : Application(){
     open fun initializeComponent(): AppComponent {
         // Creates an instance of AppComponent using its Factory constructor
         // We pass the applicationContext that will be used as Context in the graph
+        dfgdfgdfgdg
         return DaggerAppComponent.factory().create(applicationContext)
     }
 }
